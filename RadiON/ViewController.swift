bdccf05753db869078dc28a002f785ef47e1dc31
ictechgy <ictechgy@gmail.com>
2020-11-11 15:10:31 +0900

    iOS Swift App

---
+++ RadiON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RadiON
+//
+//  Created by JINHONG AN on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
